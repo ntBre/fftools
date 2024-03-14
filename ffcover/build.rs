04ba70ded72abc57c5be78eab86f9728771e9efd
Brent Westbrook <brentrwestbrook@gmail.com>
2024-03-14 15:56:05 -0400

    resolve linking issue, I guess I will need build scripts again

--- ffcover/build.rs
+++ ffcover/build.rs
@@ -2,5 +2,9 @@ fn main() {
     println!("cargo:rerun-if-env-changed=CONDA_PREFIX");
     let prefix = std::env::var("CONDA_PREFIX").expect("build in conda env");
     let conda = std::path::Path::new(&prefix).join("lib");
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", conda.display());
+    let out = std::env::var("DEP_SHIM2_INCLUDE").unwrap();
+    println!(
+        "cargo:rustc-link-arg=-Wl,-rpath,{},-rpath,{out}",
+        conda.display()
+    );
 }
